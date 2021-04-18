ae4e27d274e9e9f03eadf851fbc2a3dda4388421
Juan Pedro Paredes Caballero <juanpedro.paredes@gmail.com>
2021-04-18 20:38:01 +0200

    fix network

--- scripts/getTip.sh
+++ scripts/getTip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CARDANO_NODE_SOCKET_PATH="$NODE_HOME/sockets/node.socket" /usr/local/bin/cardano-cli query tip --$NODE_CONFIG
+CARDANO_NODE_SOCKET_PATH="$NODE_HOME/sockets/node.socket" /usr/local/bin/cardano-cli query tip --$NODE_NETWORK
