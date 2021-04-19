7e474db9d1483cc5b950a982d0a3be9a14671d02
Juan Pedro Paredes Caballero <juanpedro.paredes@gmail.com>
2021-04-19 15:14:49 +0200

    fix iowait

--- scripts/healthCheck.sh
+++ scripts/healthCheck.sh
@@ -24,19 +24,26 @@ then
     TIMEOUT=120
 fi
 
+#first check io wait
+IOWAIT=$(vmstat 10 2|tail -1|awk '{print $16}')
+if [ "$IOWAIT" -gt "90" ]
+then
+    TIMERANGE=$((($TIMEOUT*3)-15))
+    #seem iowait is high, go to see if in TIMERANGE time range is high too
+    IOWAIT=$(vmstat $TIMERANGE 2|tail -1|awk '{print $16}')
+    if [ "$IOWAIT" -gt "95" ]
+    then
+        exit 1
+    fi
+fi
+
+#second check cardano tip
 export CARDANO_NODE_SOCKET_PATH="$NODE_HOME/sockets/node.socket"
 if [ -S "$CARDANO_NODE_SOCKET_PATH" ]
 then
     #started and respond to query
     timeout $TIMEOUT /usr/local/bin/cardano-cli query tip --${NODE_NETWORK} > /dev/null 2>&1
     ERROR=$?
-    # echo "TIMEOUT $TIMEOUT CODE $ERROR"
-    # if [ "$ERROR" -eq "1"  ]
-    # then
-    #     #socket not ready
-    #     echo "EXIT 0"
-    #     exit 0
-    # fi
     exit $ERROR
 else
     #There aren't socket, not started yet
