b3a3db105824b06207bb20aaf84a361d58458735
Juan Pedro Paredes Caballero <juanpedro.paredes@gmail.com>
2021-05-06 08:41:13 +0200

    prepare for riscv64 architecture

--- util/install-deb.sh
+++ util/install-deb.sh
@@ -2,6 +2,7 @@
 ARCH=$1
 
 echo "Download Debs for $ARCH"
+cat /etc/apt/sources.list|sed -e 's/^deb /deb-src /' >> /etc/apt/sources.list
 apt-get -y update 
 apt-get -y upgrade
 
@@ -16,4 +17,9 @@ case $ARCH in
         libffi-dev libgmp-dev libssl-dev libtinfo-dev libsystemd-dev zlib1g-dev make g++ tmux git jq wget libncursesw5 libtool \
         autoconf
         ;;
+    riscv64)
+		apt-get -y install --no-install-recommends apt-utils bash curl wget ca-certificates automake build-essential pkg-config \
+        libffi-dev libgmp-dev libssl-dev libtinfo-dev libsystemd-dev zlib1g-dev make g++ tmux git jq wget libncursesw5 libtool \
+        autoconf libnuma1 libnuma-dev llvm-9 llvm-9-dev
+	    ;;
 esac
